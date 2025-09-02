ba389274e4a1927ae89db5e8f850ef482bd8f105
musta <Mustafa10000@live.dk>
2025-09-02 12:02:46 +0200

    aflevering1

---
+++ Server/InMemoryRepositories/CommentInMemoryRepository.cs
@@ -0,0 +1,50 @@
+﻿using Entities;
+using RepositoryContracts;
+
+namespace InMemoryRepositories;
+
+
+public class CommentInMemoryRepository : ICommentRepository
+{
+    private readonly List<Comment> comments = new List<Comment>();
+
+    public Task<Comment> AddAsync(Comment comment)
+    {
+        comment.Id = comments.Any() ? comments.Max(c => c.Id) + 1 : 1;
+        comments.Add(comment);
+        return Task.FromResult(comment);
+    }
+
+    public Task UpdateAsync(Comment comment)
+    {
+        var existing = comments.SingleOrDefault(c => c.Id == comment.Id);
+        if (existing is null)
+            throw new InvalidOperationException($"Comment with ID '{comment.Id}' not found");
+
+        comments.Remove(existing);
+        comments.Add(comment);
+        return Task.CompletedTask;
+    }
+
+    public Task DeleteAsync(int id)
+    {
+        var toRemove = comments.SingleOrDefault(c => c.Id == id);
+        if (toRemove is null)
+            throw new InvalidOperationException($"Comment with ID '{id}' not found");
+
+        comments.Remove(toRemove);
+        return Task.CompletedTask;
+    }
+
+    public Task<Comment> GetSingleAsync(int id)
+    {
+        var comment = comments.SingleOrDefault(c => c.Id == id);
+        if (comment is null)
+            throw new InvalidOperationException($"Comment with ID '{id}' not found");
+
+        return Task.FromResult(comment);
+    }
+
+    public IQueryable<Comment> GetMany() => comments.AsQueryable();
+}
+    
